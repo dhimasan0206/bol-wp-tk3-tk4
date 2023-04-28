7ba20e12eb002469719236a77c7491fd1687ffba
Dhimas Anugrah Dwi Yunidar <dhimasanugrahdwi@gmail.com>
2023-04-28 22:40:44 +0700

    update database seeder

--- README.md
+++ README.md
@@ -9,11 +9,28 @@
 5. `docker-compose exec myapp php artisan adminlte:install --only=auth_views`
 6. `docker-compose exec myapp php artisan adminlte:plugins install --plugin=icheckBootstrap`
 7. `docker-compose exec myapp npm install`
-8. `docker-compose exec myapp npm run dev` or `docker-compose exec myapp npm run build`
+8. `docker-compose exec myapp npm run dev` atau `docker-compose exec myapp npm run build`
 9. `docker-compose exec myapp composer require spatie/laravel-permission`
 10. `docker-compose exec myapp php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider"`
-11. `docker-compose exec myapp php artisan migrate:fresh`
-12. tambah `use Spatie\Permission\Traits\HasRoles;` ke `app/Models/User.php`
-13. `docker-compose exec myapp php artisan permission:create-role staff web "manage order|manage product|manage customer"`
-14. `docker-compose exec myapp php artisan permission:create-role admin web "manage product|manage user"`
-15. `docker-compose exec myapp php artisan permission:create-role customer web "create order"`
+11. tambah `use Spatie\Permission\Traits\HasRoles;` ke `app/Models/User.php`
+12. tambah permission, role dan user seeder di DatabaseSeeder
+13. `docker-compose exec myapp php artisan migrate:fresh --seed`
+
+## local deployment
+
+1. `docker-compose up` or `docker-compose up -d`
+2. `docker-compose exec myapp php artisan migrate:fresh --seed`
+3. buka `http://localhost:8000` atau `http://0.0.0.0:8000`
+4. Login sebagai:
+   1. Super Admin:
+      1. email: super.admin@laravel.com
+      2. password: superadmin1234
+   2. Admin:
+      1. email: admin@laravel.com
+      2. password: admin1234
+   3. Staff
+      1. email: staff@laravel.com
+      2. password: staff1234
+   4. Customer:
+      1. email: customer@laravel.com
+      2. password: customer1234
--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +25,42 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'manage user']);
+        Permission::create(['name' => 'manage customer']);
+        Permission::create(['name' => 'manage product']);
+        Permission::create(['name' => 'manage order']);
+        Permission::create(['name' => 'create order']);
+
+        Role::create(['name' => 'super-admin'])->givePermissionTo(Permission::all());
+        Role::create(['name' => 'admin'])->givePermissionTo(['manage user', 'manage product']);
+        Role::create(['name' => 'staff'])->givePermissionTo(['manage customer', 'manage product', 'manage order']);
+        Role::create(['name' => 'customer'])->givePermissionTo(['create order']);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'super.admin@laravel.com',
+            'password' => Hash::make('superadmin1234'),
+        ])->assignRole('super-admin');
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@laravel.com',
+            'password' => Hash::make('admin1234'),
+        ])->assignRole('admin');
+
+        User::create([
+            'name' => 'Staff',
+            'email' => 'staff@laravel.com',
+            'password' => Hash::make('staff1234'),
+        ])->assignRole('staff');
+
+        User::create([
+            'name' => 'Customer',
+            'email' => 'customer@laravel.com',
+            'password' => Hash::make('customer1234'),
+        ])->assignRole('customer');
     }
 }
